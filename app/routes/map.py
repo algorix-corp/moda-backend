6053e4cfd65cd9c58568f1e1564af25573e2f4dd
Ryan Kwon <ryankwon@algorix.io>
2023-11-03 01:44:37 +0900

    route_drt update

--- app/routes/map.py
+++ app/routes/map.py
@@ -49,6 +49,7 @@ def route_drt(start_poi: str, end_poi: str):
     end_lon = float(end_loc["poiDetailInfo"]["lon"])
 
     route = route_search(start_lat, start_lon, end_lat, end_lon)
+    drt_count = 0
     # print(route)
 
     for i in range(len(route["metaData"]["plan"]["itineraries"][0]["legs"])):
@@ -56,29 +57,39 @@ def route_drt(start_poi: str, end_poi: str):
                 route["metaData"]["plan"]["itineraries"][0]["legs"][i]["sectionTime"] > 600:
             route["metaData"]["plan"]["itineraries"][0]["legs"][i]["mode"] = "DRT"
             route["metaData"]["plan"]["itineraries"][0]["legs"][i]["mention"] = (f'DRT를 사용해 '
-            f'{route["metaData"]["plan"]["itineraries"][0]["legs"][i]["start"]["name"]}에서 '
-            f'{route["metaData"]["plan"]["itineraries"][0]["legs"][i]["end"]["name"]}까지 이동합니다.')
+                                                                                 f'{route["metaData"]["plan"]["itineraries"][0]["legs"][i]["start"]["name"]}에서 '
+                                                                                 f'{route["metaData"]["plan"]["itineraries"][0]["legs"][i]["end"]["name"]}까지 이동합니다.')
+            drt_count += 1
 
     try:
         fareWon = route["metaData"]["plan"]["itineraries"][0]["fare"]["regular"]["totalFare"]
         timeMinute = route["metaData"]["plan"]["itineraries"][0]["totalTime"]
 
-        return {
-            "route": route["metaData"]["plan"]["itineraries"][0]["legs"],
-            "fareWon": fareWon + 1500,
-            "timeMinute": timeMinute / 60,
-            "savedWon": "3500",
-            "savedMinute": "20"
-        }
+        if drt_count != 0:
+            return {
+                "route": route["metaData"]["plan"]["itineraries"][0]["legs"],
+                "fareWon": fareWon + 1500,
+                "timeMinute": timeMinute / 60,
+                "savedWon": "3500",
+                "savedMinute": "20"
+            }
+        else:
+            return {
+                "route": route["metaData"]["plan"]["itineraries"][0]["legs"],
+                "fareWon": fareWon,
+                "timeMinute": timeMinute / 60,
+                "savedWon": "0",
+                "savedMinute": "0"
+            }
     except KeyError:
         return {
             "route": route["metaData"]["plan"]["itineraries"][0]["legs"],
             "fareWon": -1,
             "timeMinute": -1,
             "savedWon": -1,
-            "savedMinute": -1
+            "savedMinute": -1,
+            "drt_count": drt_count
         }
-
 
 # resp = route_drt("1866614", "535438")
 # print(resp)
